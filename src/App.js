f38fa262cae4631076fbf8aada8a0c7c27fb0406
Abhinav K K <91086614+Abhinav-kk@users.noreply.github.com>
2023-01-12 20:20:11 +0400

    Final Commit

---
+++ src/App.js
@@ -0,0 +1,218 @@
+import { useRef, useState, useEffect } from "react"
+import { Canvas, useFrame } from "@react-three/fiber"
+import { useGLTF, ContactShadows, Environment, OrbitControls } from "@react-three/drei"
+import { HexColorPicker } from "react-colorful"
+import { proxy, useSnapshot } from "valtio"
+import Button from '@mui/material/Button';
+import './styles.css';
+
+let buttonValue = "";
+
+//Shirt
+const state = proxy({
+  current: null,
+  items: {
+    Front: "#ffffff",
+    Back: "#ffffff",
+    CollarFront: "#ffffff",
+    CollarBack: "#ffffff",
+    LeftArm: "#ffffff",
+    RightArm: "#ffffff",
+    BlackPart: "#000000",
+    WhitePart: "#ffffff",
+    CupInside: "#ffffff",
+    CupOutside: "#ff0000",
+    ChairLegs: "#000000",
+    ChairSeat: "#0000ff"
+  },
+})
+
+function Shirt() {
+  const ref = useRef()
+  const snap = useSnapshot(state)
+  const { nodes, materials } = useGLTF("tshirt.glb")
+  const [hovered, set] = useState(null)
+
+  useFrame((state) => {
+    ref.current.position.y = (-6 + Math.sin(60 / 1.5)) / 10
+  })
+
+  useEffect(() => {
+    const cursor = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><g filter="url(#filter0_d)"><path d="M29.5 47C39.165 47 47 39.165 47 29.5S39.165 12 29.5 12 12 19.835 12 29.5 19.835 47 29.5 47z" fill="${snap.items[hovered]}"/></g><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/><text fill="#000" style="white-space:pre" font-family="Inter var, sans-serif" font-size="10" letter-spacing="-.01em"><tspan x="35" y="63">${hovered}</tspan></text></g><defs><clipPath id="clip0"><path fill="#fff" d="M0 0h64v64H0z"/></clipPath><filter id="filter0_d" x="6" y="8" width="47" height="47" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="2"/><feGaussianBlur stdDeviation="3"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/><feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>`
+    const auto = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/></svg>`
+    if (hovered) {
+      document.body.style.cursor = `url('data:image/svg+xml;base64,${btoa(cursor)}'), auto`
+      return () => (document.body.style.cursor = `url('data:image/svg+xml;base64,${btoa(auto)}'), auto`)
+    }
+  }, [hovered])
+
+  console.log(nodes, materials);
+  return (
+    <group
+      ref={ref}
+      onPointerOver={(e) => (e.stopPropagation(), set(e.object.material.name))}
+      onPointerOut={(e) => e.intersections.length === 0 && set(null)}
+      onPointerMissed={() => (state.current = null)}
+      onClick={(e) => (e.stopPropagation(), (state.current = e.object.material.name))}>
+      <mesh receiveShadow castShadow geometry={nodes.Front.geometry} material={materials.Front} material-color={snap.items.Front} />
+      <mesh receiveShadow castShadow geometry={nodes.Back.geometry} material={materials.Back} material-color={snap.items.Back} />
+      <mesh receiveShadow castShadow geometry={nodes.CollarFront.geometry} material={materials.CollarFront} material-color={snap.items.CollarFront} />
+      <mesh receiveShadow castShadow geometry={nodes.CollarBack.geometry} material={materials.CollarBack} material-color={snap.items.CollarBack} />
+      <mesh receiveShadow castShadow geometry={nodes.LeftArm.geometry} material={materials.LeftArm} material-color={snap.items.LeftArm} />
+      <mesh receiveShadow castShadow geometry={nodes.RightArm.geometry} material={materials.RightArm} material-color={snap.items.RightArm} />
+    </group>
+  )
+}
+
+//FootBall
+function FootBall() {
+  const ref = useRef()
+  const snap = useSnapshot(state)
+  const { nodes, materials } = useGLTF("ball.glb")
+  const [hovered, set] = useState(null)
+
+  useFrame((state) => {
+    ref.current.position.y = (-6 + Math.sin(60 / 1.5)) / 10
+  })
+
+  useEffect(() => {
+    const cursor = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><g filter="url(#filter0_d)"><path d="M29.5 47C39.165 47 47 39.165 47 29.5S39.165 12 29.5 12 12 19.835 12 29.5 19.835 47 29.5 47z" fill="${snap.items[hovered]}"/></g><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/><text fill="#000" style="white-space:pre" font-family="Inter var, sans-serif" font-size="10" letter-spacing="-.01em"><tspan x="35" y="63">${hovered}</tspan></text></g><defs><clipPath id="clip0"><path fill="#fff" d="M0 0h64v64H0z"/></clipPath><filter id="filter0_d" x="6" y="8" width="47" height="47" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="2"/><feGaussianBlur stdDeviation="3"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/><feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>`
+    const auto = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/></svg>`
+    if (hovered) {
+      document.body.style.cursor = `url('data:image/svg+xml;base64,${btoa(cursor)}'), auto`
+      return () => (document.body.style.cursor = `url('data:image/svg+xml;base64,${btoa(auto)}'), auto`)
+    }
+  }, [hovered])
+
+  console.log(nodes, materials);
+  return (
+    <group
+      ref={ref}
+      onPointerOver={(e) => (e.stopPropagation(), set(e.object.material.name))}
+      onPointerOut={(e) => e.intersections.length === 0 && set(null)}
+      onPointerMissed={() => (state.current = null)}
+      onClick={(e) => (e.stopPropagation(), (state.current = e.object.material.name))}>
+      <mesh geometry={nodes.WhitePart.geometry} material={materials.WhitePart} material-color={snap.items.WhitePart} />
+      <mesh geometry={nodes.BlackPart.geometry} material={materials.BlackPart} material-color={snap.items.BlackPart} />
+    </group>
+  )
+}
+
+//Cup
+function Cup() {
+  const ref = useRef()
+  const snap = useSnapshot(state)
+  const { nodes, materials } = useGLTF("cup.glb")
+  const [hovered, set] = useState(null)
+
+  useFrame((state) => {
+    ref.current.position.y = (-6 + Math.sin(60 / 1.5)) / 10
+  })
+
+  useEffect(() => {
+    const cursor = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><g filter="url(#filter0_d)"><path d="M29.5 47C39.165 47 47 39.165 47 29.5S39.165 12 29.5 12 12 19.835 12 29.5 19.835 47 29.5 47z" fill="${snap.items[hovered]}"/></g><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/><text fill="#000" style="white-space:pre" font-family="Inter var, sans-serif" font-size="10" letter-spacing="-.01em"><tspan x="35" y="63">${hovered}</tspan></text></g><defs><clipPath id="clip0"><path fill="#fff" d="M0 0h64v64H0z"/></clipPath><filter id="filter0_d" x="6" y="8" width="47" height="47" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="2"/><feGaussianBlur stdDeviation="3"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/><feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>`
+    const auto = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/></svg>`
+    if (hovered) {
+      document.body.style.cursor = `url('data:image/svg+xml;base64,${btoa(cursor)}'), auto`
+      return () => (document.body.style.cursor = `url('data:image/svg+xml;base64,${btoa(auto)}'), auto`)
+    }
+  }, [hovered])
+
+  console.log(nodes, materials);
+  return (
+    <group
+      ref={ref}
+      onPointerOver={(e) => (e.stopPropagation(), set(e.object.material.name))}
+      onPointerOut={(e) => e.intersections.length === 0 && set(null)}
+      onPointerMissed={() => (state.current = null)}
+      onClick={(e) => (e.stopPropagation(), (state.current = e.object.material.name))}>
+      <mesh geometry={nodes.CupOutside.geometry} material={materials.CupOutside} material-color={snap.items.CupOutside} />
+      <mesh geometry={nodes.CupInside.geometry} material={materials.CupInside} material-color={snap.items.CupInside} />
+    </group>
+  )
+}
+
+//Cup
+function Chair() {
+  const ref = useRef()
+  const snap = useSnapshot(state)
+  const { nodes, materials } = useGLTF("chair.glb")
+  const [hovered, set] = useState(null)
+
+  useFrame((state) => {
+    ref.current.position.y = (-6 + Math.sin(60 / 1.5)) / 10
+  })
+
+  useEffect(() => {
+    const cursor = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><g clip-path="url(#clip0)"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><g filter="url(#filter0_d)"><path d="M29.5 47C39.165 47 47 39.165 47 29.5S39.165 12 29.5 12 12 19.835 12 29.5 19.835 47 29.5 47z" fill="${snap.items[hovered]}"/></g><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/><text fill="#000" style="white-space:pre" font-family="Inter var, sans-serif" font-size="10" letter-spacing="-.01em"><tspan x="35" y="63">${hovered}</tspan></text></g><defs><clipPath id="clip0"><path fill="#fff" d="M0 0h64v64H0z"/></clipPath><filter id="filter0_d" x="6" y="8" width="47" height="47" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB"><feFlood flood-opacity="0" result="BackgroundImageFix"/><feColorMatrix in="SourceAlpha" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/><feOffset dy="2"/><feGaussianBlur stdDeviation="3"/><feColorMatrix values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.15 0"/><feBlend in2="BackgroundImageFix" result="effect1_dropShadow"/><feBlend in="SourceGraphic" in2="effect1_dropShadow" result="shape"/></filter></defs></svg>`
+    const auto = `<svg width="64" height="64" fill="none" xmlns="http://www.w3.org/2000/svg"><path fill="rgba(255, 255, 255, 0.5)" d="M29.5 54C43.031 54 54 43.031 54 29.5S43.031 5 29.5 5 5 15.969 5 29.5 15.969 54 29.5 54z" stroke="#000"/><path d="M2 2l11 2.947L4.947 13 2 2z" fill="#000"/></svg>`
+    if (hovered) {
+      document.body.style.cursor = `url('data:image/svg+xml;base64,${btoa(cursor)}'), auto`
+      return () => (document.body.style.cursor = `url('data:image/svg+xml;base64,${btoa(auto)}'), auto`)
+    }
+  }, [hovered])
+
+  console.log(nodes, materials);
+  return (
+    <group
+      ref={ref}
+      onPointerOver={(e) => (e.stopPropagation(), set(e.object.material.name))}
+      onPointerOut={(e) => e.intersections.length === 0 && set(null)}
+      onPointerMissed={() => (state.current = null)}
+      onClick={(e) => (e.stopPropagation(), (state.current = e.object.material.name))}>
+      <mesh geometry={nodes.ChairLegs.geometry} material={materials.ChairLegs} material-color={snap.items.ChairLegs} />
+      <mesh geometry={nodes.ChairSeat.geometry} material={materials.ChairSeat} material-color={snap.items.ChairSeat} />
+    </group>
+  )
+}
+
+function Picker() {
+  const snap = useSnapshot(state);
+
+  return (
+    <div style={{ display: snap.current ? "block" : "none" }}>
+      <HexColorPicker className="picker" color={snap.items[snap.current]} onChange={(color) => (state.items[snap.current] = color)} />
+      <h2 className="pickerText">{snap.current}</h2>
+    </div>
+  )
+}
+
+export default function App() {
+  const [model, setModel] = useState('');
+  const handleClick = (element) => {
+    buttonValue = element.target.value
+    console.log("Clicked" + element.target.value);
+    if (buttonValue === "Shirt") {
+      setModel(<Shirt />);
+    } else if (buttonValue === "FootBall") {
+      setModel(<FootBall />);
+    } else if (buttonValue === "Cup") {
+      setModel(<Cup />);
+    } else if (buttonValue === "Chair") {
+      setModel(<Chair />);
+    }
+  };
+
+  return (
+    <>
+      <div className="selectModel">
+        <h2 style={{ fontsize: "25px" }}>Select Model</h2>
+        <Button sx={{ m: 2 }} onClick={handleClick} variant="contained" value="Shirt">Shirt</Button>
+        <Button sx={{ m: 2 }} onClick={handleClick} variant="contained" value="FootBall">FootBall</Button>
+        <Button sx={{ m: 2 }} onClick={handleClick} variant="contained" value="Cup">Cup</Button>
+        <Button sx={{ m: 2 }} onClick={handleClick} variant="contained" value="Chair">Chair</Button>
+      </div>
+      <Canvas shadows camera={{ position: [0, 0, 8], fov: 20 }}>
+        <ambientLight intensity={0.7} />
+        <spotLight intensity={0.5} angle={0.1} penumbra={1} position={[10, 15, 10]} castShadow />
+        {model}
+        <Environment preset="city" />
+        <ContactShadows position={[0, -0.8, 0]} opacity={0.25} scale={10} blur={1.5} far={0.8} />
+        <OrbitControls minPolarAngle={0} enableRotate={true} enablePan={false} enableZoom={false} />
+      </Canvas>
+      <Picker />
+      <h2 className="credits">Webpage Built By <a href="https://abhinavkk.me">Abhinav K.K</a> for Zigy Technical Assignment</h2>
+      <h2 className="credits2">Models From <a href="https://sketchfab.com/">SketchFab</a></h2>
+    </>
+  )
+}
